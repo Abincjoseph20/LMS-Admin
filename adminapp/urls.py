099eea6e0ef2e367ea6f62eddde323638bd2bd78
Abincjoseph20 <ebincjoseph2@gmail.com>
2025-07-18 15:34:31 +0530

    chaage log

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path('', views.register, name='register'),
     path('verify-otp/', views.verify_otp, name='verify_otp'),
     path('login/', views.login, name='login'),
